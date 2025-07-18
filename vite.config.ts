b3441e3e6ec74c5cc15ffb7fbc9d3fff5b14ab33
dev.jorge.c <dev.jorge.c@gmail.com>
2025-07-18 06:11:57 +0000

    fixing vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    hmr: {
+      host: '9000-firebase-monthlyearthday2git-1752817683361.cluster-uf6urqn4lned4spwk4xorq6bpo.cloudworkstations.dev',
+      clientPort: 9000,
+    },
+  },
+});
